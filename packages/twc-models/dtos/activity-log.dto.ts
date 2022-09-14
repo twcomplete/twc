700cd3d1e3451245d511d69cc2be37c9fa599994
ambrosiak <kyle.kim0305@gmail.com>
2022-09-14 17:12:15 +0900

    Create Dtos

---
+++ packages/twc-models/dtos/activity-log.dto.ts
@@ -0,0 +1,6 @@
+export class ActivityLog {
+    _id: number;
+    userId: number;
+    wtime: Date;
+    mtime: Date;
+}
---
+++ packages/twc-models/dtos/attendance-info.dto.ts
@@ -0,0 +1,5 @@
+export class AttendaceInformation {
+    _id: number;
+    userId: number;
+    attendTime: Date;
+}
