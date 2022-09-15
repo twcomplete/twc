194d6f058c3616676b64165b9ad098d8f6bfaf1c
21color <lehhha0110@gmail.com>
2022-09-15 18:57:42 +0900

    test

---
+++ packages/twc-front/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './style/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>오운완</h1>
+    </div>
+  )
+}
+
+export default App
