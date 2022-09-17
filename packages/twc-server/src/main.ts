fb294642dd19ee9b77448c139b93d6f650ec633c
ambrosiak <kyle.kim0305@gmail.com>
2022-09-17 15:32:37 +0900

    Create nestjs-redoc

--- packages/twc-server/src/main.ts
+++ packages/twc-server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { RedocOptions, RedocModule } from 'nestjs-redoc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,23 @@ async function bootstrap() {
     .addTag('ㅇㅇㅇ')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  // SwaggerModule.setup('docs', app, document);
+
+  // nestjs-redoc 설정
+  // 설정 상세: https://www.npmjs.com/package/nestjs-redoc
+  const redocOptions: RedocOptions = {
+    title: '#ㅇㅇㅇ',
+
+    sortPropsAlphabetically: false,
+
+    hideDownloadButton: false,
+    hideHostname: false,
+
+    requiredPropsFirst: false,
+
+    expandResponses: 'all',
+  };
+  await RedocModule.setup('/docs', app, document, redocOptions);
 
   await app.listen(3000);
 }
