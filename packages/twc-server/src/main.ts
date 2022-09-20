d58919629011042772ec71869b475c8b6c2884bf
ambrosiak <kyle.kim0305@gmail.com>
2022-09-20 21:44:04 +0900

    Model update

--- packages/twc-server/src/main.ts
+++ packages/twc-server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
