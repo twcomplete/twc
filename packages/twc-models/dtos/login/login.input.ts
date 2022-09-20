5cac3b041e9143fd0810922fecab79dce94b17d2
ambrosiak <kyle.kim0305@gmail.com>
2022-09-20 21:58:04 +0900

    API docs update

--- packages/twc-models/dtos/login/login.input.ts
+++ packages/twc-models/dtos/login/login.input.ts
@@ -1,4 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 export class LoginInput {
+    @ApiProperty({
+        description: '아이디',
+        example: 'hana',
+    })
+    @Expose()
     id: string;
+
+    @ApiProperty({
+        description: '비밀번호',
+        example: '21color',
+    })
+    @Expose()
     pwd: string;
 }
--- packages/twc-server/src/users/users.controller.ts
+++ packages/twc-server/src/users/users.controller.ts
@@ -1,17 +1,35 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@twc/twc-models';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags('유저 정보')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({
+    summary: '유저 정보 가져오기',
+    description: '가입 유저 전체 리스트 가져오기',
+  })
+  @ApiResponse({
+    status: 200,
+    type: [User],
+    description: '유저 리스트',
+  })
   @Get()
   getUsers(): string {
     return this.usersService.getUsers();
   }
 
+  @ApiOperation({
+    summary: '유저 정보 입력하기',
+    description: '회원 가입 또는 유저 정보 수동 입력',
+  })
+  @ApiResponse({
+    status: 200,
+    type: User,
+    description: '유저 정보',
+  })
   @Post()
   save(@Body() input) {
     return this.usersService.save(input);
