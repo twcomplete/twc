3ee5dadad57c7f89318d22832adfd354dbbe4844
ambrosiak <kyle.kim0305@gmail.com>
2022-09-21 23:27:28 +0900

    Make schema module

--- packages/twc-server/src/attend/attend.module.ts
+++ packages/twc-server/src/attend/attend.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DbModule } from 'src/lib/db/db.module';
+import { AttendSchema } from 'src/lib/interfaces/attend.interface';
 import { AttendController } from './attend.controller';
 import { attendsProviders } from './attend.provider';
 import { AttendService } from './attend.service';
 
 @Module({
-  imports: [DbModule],
-  providers: [AttendService, ...attendsProviders],
+  imports: [DbModule, MongooseModule.forFeature([{ name: AttendSchema.id }])],
+  providers: [AttendService],
   controllers: [AttendController],
 })
 export class AttendModule {}
