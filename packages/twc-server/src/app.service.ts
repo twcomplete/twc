31250f41b2e0004a7bf88a43ee8079b391268a3d
ambrosiak <kyle.kim0305@gmail.com>
2022-09-25 23:19:27 +0900

    update

--- packages/twc-server/src/app.service.ts
+++ packages/twc-server/src/app.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 @Injectable()
 export class AppService {
-
   healthCheck(): string {
-    return 'Hello World! I am healthy';
+    return 'OK';
   }
 }
